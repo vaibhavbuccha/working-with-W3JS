8b6d502f0bdc29446cf844f51e532e6f13a9e6fc
vaibhavbuccha <jainvaibhav415@gmail.com>
2018-07-08 20:21:51 +0530

    Create w3-js.js
    w3-js script

---
+++ w3-js.js
@@ -0,0 +1,4 @@
+	<script src="https://www.w3schools.com/lib/w3.js"></script>
+  //this is the script url 
+  //via adding this in our web page code we can access w3-js.
+  //thank you "happy coding"
